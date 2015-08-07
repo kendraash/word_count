1e8012c6af294c34c434fcf964ce5cf82159673c
Kendra Ash <kendra.ash32@gmail.com>
2015-08-07 09:58:10 -0700

    Adding second spec which ends with a period instead of a questionmark and takes into a acount if the first letter of the word is capital.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,9 +1,20 @@
 class String
   define_method(:word_count) do
     word_count=0
-    input=self.split("?")
+    sentence_end_puncuation= ["?", ".","!"]
+    # if(self.include?(sentence_end_puncuation))
+    if self.include?(sentence_end_puncuation[0])
+      input=self.split(sentence_end_puncuation[0])
+    elsif self.include?(sentence_end_puncuation[1])
+      input=self.split(sentence_end_puncuation[1])
+    else
+      input=self.split(sentence_end_puncuation[2])
+    end
     puts input
     sentence=input[0].split(" ")
+    # if sentence[0]==[/[A-Z]+/]
+    sentence[0].downcase!()
+
     word=input[1].split(" ")
     puts word
     sentence.length().times do |i|
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,5 +6,8 @@ describe('String#word_count') do
   it("It will take a sentence, followed by a word, and calculate the number of times the word appears in teh sentence.") do
     expect("If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked? peck".word_count()).to(eq(2))
   end
+  it("It will take a sentence, followed by a word, and calculate the number of times the word appears in teh sentence.") do
+    expect("Twinkle twinkle little star. twinkle".word_count()).to(eq(2))
+  end
 
 end
